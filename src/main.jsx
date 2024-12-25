fa8737eecb65558907b4d591eac72e79e6f0e275
kunalambarte17 <ambartekunal17@gmail.com>
2024-12-25 21:05:10 +0530

    Some Changes Were Made

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import StoreContextProvider from './Context/StoreContext.jsx';
-
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
